08c2051fa1fd63d86c740f6ba40d6a0c4d0b9683
mprocopio1-max <m.procopio1@ied.edu>
2026-04-09 15:33:53 +0200

    cam+mic

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let video
+let audio
+
 function setup() {
   createCanvas(400, 400);
+  video = createCapture(VIDEO);
+  video.size(400, 400);
+  video.hide();
+
+  audio = new p5.AudioIn();
+  audio.start();
 }
 
 function draw() {
   background(220);
+  let volume = audio.getLevel();
+  let size = map(volume, 0, 1, 10, 200);
+  circle(200, 200, size);
+
+  let threshold = map(volume, 0, 0.005, 0, 1);
+  image(video, 0, 0);
+  filter(THRESHOLD, threshold);
 }
