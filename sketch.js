90b8603eb6a6d67835bf0d19918e907957b365fb
mprocopio1-max <m.procopio1@ied.edu>
2026-04-09 16:24:56 +0200

    EFFETTO 1

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
-let video
-let audio
+let video;
+let audio;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 400);
   video = createCapture(VIDEO);
-  video.size(400, 400);
+  video.size(500, 400);
   video.hide();
 
   audio = new p5.AudioIn();
@@ -12,12 +12,36 @@ function setup() {
 }
 
 function draw() {
-  background(220);
   let volume = audio.getLevel();
-  let size = map(volume, 0, 1, 10, 200);
-  circle(200, 200, size);
+  // console.log(volume);
+  colorKey(video, 255, 0, 0, 200); // Mantieni solo i pixel rossi con una tolleranza di 200
+  image(video, 0, 0, 500, 400);
+  
+}
 
-  let threshold = map(volume, 0, 0.005, 0, 1);
-  image(video, 0, 0);
-  filter(THRESHOLD, threshold);
+// Funzione colorKey: mantiene i pixel di un colore specifico e scarta gli altri
+// targetR, targetG, targetB: il colore da mantenere (0-255 per ogni canale)
+// threshold: tolleranza della somiglianza al colore (0-255, valori più alti = più permissivi)
+function colorKey(videoFeed, targetR, targetG, targetB, threshold) {
+  videoFeed.loadPixels();
+  
+  for (let i = 0; i < videoFeed.pixels.length; i += 4) {
+    let r = videoFeed.pixels[i];     // canale rosso
+    let g = videoFeed.pixels[i + 1]; // canale verde
+    let b = videoFeed.pixels[i + 2]; // canale blu
+    
+    // Calcola la distanza euclidea dal colore target
+    let distance = sqrt(
+      pow(r - targetR, 2) + 
+      pow(g - targetG, 2) + 
+      pow(b - targetB, 2)
+    );
+    
+    // Se la distanza è maggiore del threshold, rendi il pixel trasparente
+    if (distance > threshold) {
+      videoFeed.pixels[i + 3] = 0; // alpha = 0 (trasparente)
+    }
+  }
+  
+  videoFeed.updatePixels();
 }
